c732ca58b41f53d24fbe8a7818360183f9681312
Rachit Garg <rachitgarg1208@gmail.com>
2024-08-24 22:34:04 +0530

    C++ basics

---
+++ IfElse.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int age;
+    cin >> age;
+    if (age >= 18)
+    {
+        cout << "you are adult";
+    }
+    else
+    {
+        cout << "You are minor";
+    }
+    return 0;
+}
---
+++ basic_IO.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hey Rachit!" << "\n";
+    cout << "Hey Rachit!" << endl;
+
+    // taking input in x
+
+    int x;
+    cin >> x;
+    cout << "value of x is " << x << "\n";
+
+    int y, z;
+    cin >> y >> z;
+    cout << "value of y and z are\t" << y << "and" << z;
+
+    return 0;
+}
