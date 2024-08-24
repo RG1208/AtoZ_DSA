05e7bc958d8246e0cf53de27f0b43f74f3fd2d3f
Rachit Garg <rachitgarg1208@gmail.com>
2024-08-25 00:05:52 +0530

    Array and switch

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    cin >> arr[1] >> arr[2] >> arr[3] >> arr[4] >> arr[5];
+
+    cout << arr[1] << "\n";
+
+    arr[2] += 10;
+    cout << arr[2] << "\n";
+
+    arr[3] = 13;
+    cout << arr[3] << "\n";
+
+    return 0;
+}
