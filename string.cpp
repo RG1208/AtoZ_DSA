0bf4a1ccf254a6f3e0ebb131cdfefe7338a8bb24
Rachit Garg <rachitgarg1208@gmail.com>
2024-08-25 12:12:00 +0530

    Loops and Strings

---
+++ string.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s << "\n";
+    cout << s[2] << "\n";
+    int len = s.size();
+    cout << len;
+    return 0;
+}
---
+++ whileLoop.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    while (i <= 5)
+    {
+        cout << "Hi Rachit" << i << "\n";
+        i++;
+    }
+    return 0;
+}
