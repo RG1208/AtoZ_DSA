23d7e61868934ee5b4a4f3e5d3bc44d8f6ef35ed
Rachit Garg <rachitgarg1208@gmail.com>
2024-08-25 14:23:51 +0530

    Functions

---
+++ functions.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+void function using parameters
+
+void printName(string name)
+{
+    cout << "Hey " << name;
+}
+
+int main()
+{
+    string name;
+    cin >> name;
+    printName(name);
+    return 0;
+}
+*/
+
+void printName()
+{
+    cout << "Hey Rachit!";
+}
+int main()
+{
+    string name;
+    printName();
+    return 0;
+}
